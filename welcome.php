450eea821a9dcd3665141944c57145004c9220f2
anotoniojmendes <donutsrool@gmail.com>
2019-03-22 14:19:36 +0000

    Login Funcional

--- welcome.php
+++ welcome.php
@@ -1,46 +1,60 @@
 <?php
-session_start();
-
-if(isset($_SESSION['un'])){}
-else{header:index.html;}
+	//Este ficheiro, APÓS LOGIN (autenticação) com sucesso; apresentará uma página de boas-vindas
+	include ('session.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>Welcome</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-<!--===============================================================================================-->
-<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-</head>
-<body>
-	<table>
-	<tr>
-		<td>Name</td>
-		<td>Description</td>
-		<td>Created By</td>
-		<td>Created At</td>
-		<td>Completed At</td>
-	</tr>
-	
-	<?php
+<html>
 
-		foreach($_SESSION['un'] as $user):
-			$SqlCarrinho = mysqli_query($conect,"SELECT * FROM tasks WHERE username= '$user'");
-			echo '<tr>';
-				echo '<td>'.$ResAssoc['name'].'</td>';
-				echo '<td>'.$ResAssoc['description'].'</td>';
-				echo '<td>'.$ResAssoc['username'].'</td>'
-				echo '<td>'.$ResAssoc['created_at'].'</td>';
-				echo '<td>'.$ResAssoc['completed_at'].'</td>';				
-			echo '</tr>';
-		endforeach;
-	?>	
+	<head>
+		<title> Bem-vindo (a)!</title>
+		<meta charset = "UTF-8">
+	</head>
+
+	<body>
+		<div align = "right"; style = " font-size:12px;"><h2>
+		<p> Welcome (a) <?php echo ' '.$login_session.'<br>'; ?>
+		<a href = "logout.php"> Log Out (<i> out </i>) </a></p></h2>
+		</div>
+		
+		<?php $tasks = DBRead2()?>
+		<h1> List of Tasks</h1>
+		<table width="100%" border="1">
+			<tr> 
+			<td align=center>
+			Name
+			</td>
+			<td align=center>
+			Description
+			</td>
+			<td align=center>
+			Created By
+			</td>
+			<td align=center>
+			Created At
+			</td>
+			<td align=center>
+			Completed At
+			</td>
+			<td align=center>
+			Edit Task
+			</td>
+			<td align=center>
+			Delete Task
+			</td>
+			<?php foreach($task as $cl)
+			{?>
+			<tr> 
+			<td align=center><?php echo ($cl['name']) ?></td>
+			<td align=center><?php echo ($cl['description']) ?></td>
+			<td align=center><?php echo ($cl['username']) ?></td>
+	        <td align=center><?php echo ($cl['created_at']) ?></td>
+			<td align=center><?php echo ($cl['completed_at']) ?></td>
+			<td><a href="alterar.php?id=<?=$cl['id_task']?>">&#8634;</a></td>
+			<td><a href="eliminar.php?id=<?=$cl['id_task']?>" onclick="return confirm('Tem a certeza que pretende eliminar o registo?')">x</a></td>
+			</tr>
+			<?php } ?>
+		</table>
+		<br>
+		<a href="registar.html"> <input type="button" name="" value="Registar carro"></a>
+	</body>
 	
-	</table>
-</body>
 </html>
